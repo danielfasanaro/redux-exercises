cc2bc08da2a3d0f5c5f944fa69152bdc3be3cf89
danielfasanaro <danielfasanaro@gmail.com>
2021-04-01 15:26:38 -0300

    depois descubroi o que eu fiz

--- src/helpers/renderWithRedux.js
+++ src/helpers/renderWithRedux.js
@@ -4,9 +4,11 @@ import { render } from '@testing-library/react';
 import { createStore, combineReducers } from 'redux';
 import clickReducer from '../reducers';
 
+const createMockStore = (initialState) => createStore(combineReducers({ clickReducer }), initialState);
+
 const renderWithRedux = (
   component,
-  { initialState, store = createStore(combineReducers({ clickReducer }), initialState) } = {}
+  { initialState, store = createMockStore(initialState) } = {}
 ) => {
   return {
     ...render(<Provider store={store}>{component}</Provider>),
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,4 +1,5 @@
 import { screen, cleanup } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRedux from '../helpers/renderWithRedux'
 
@@ -10,5 +11,29 @@ describe('testing clicks', () => {
 
     expect(buttonAdicionar).toBeInTheDocument();
     expect(queryByText('0')).toBeInTheDocument();
+  });
+
+  test('the initial counter value should be 5', () => {
+    const { queryByText } = renderWithRedux(<App />, { initialState: { clickReducer: { counter: 5 }}});
+
+    expect(queryByText('5')).toBeInTheDocument();
+  });
+
+  test('a click in a button should increment the value of clicks', () => {
+    renderWithRedux(<App />);
+
+    expect(screen.getByText('0')).toBeInTheDocument();
+    const clickHereButton = screen.getByRole('button', { name: 'Clique aqui' });
+    userEvent.click(clickHereButton);
+    expect(screen.getByText('1')).toBeInTheDocument();
+  });
+
+  test('the initial value should be 10 and increment to 11 after a click', () => {
+    renderWithRedux(<App />, { initialState: { clickReducer: { counter: 10 } } });
+
+    expect(screen.getByText('10')).toBeInTheDocument();
+    const clickHereButton = screen.getByRole('button', { name: 'Clique aqui' });
+    userEvent.click(clickHereButton);
+    expect(screen.getByText('11')).toBeInTheDocument();
   });
 });
